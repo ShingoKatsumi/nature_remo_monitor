7b22e83d98329b8e0283a528b7a8f1f6b286fdf8
Shingo-Laptop <escape57@hotmail.com>
2025-08-31 21:26:33 +0900

    待ち時間設定を追加

--- import requests.py
+++ import requests.py
@@ -3,15 +3,24 @@ import time
 import datetime
 
 api = NatureRemoAPI('ory_at_9Oc0RqPImm2c0ea8rUyMVVqoKI5r_y3ncqF1iktEpiw.Zukn-6s28FGhwoOvbKHglBSThSQv1UaFhIkcKqfpAuo')
-devices = api.get_devices()
-print(devices)
 
 while True:
-    while api.rate_limit.remaining>0:
+    # デバイス情報を取得
+    devices = api.get_devices()
+    #print(devices)
+
+    if api.rate_limit.remaining>0:    
         for device in devices:
             print(device.name, str(device.newest_events['te'].val), str(device.newest_events['hu'].val) )
             print(str(api.rate_limit.limit), str(api.rate_limit.remaining),str(api.rate_limit.reset+datetime.timedelta(hours=9)))
-        time.sleep(1)
 
-    if str(api.rate_limit.reset+datetime.timedelta(hours=9)) > datetime.now():
+    #待ち時間を設定
+    times_remain = api.rate_limit.reset+datetime.timedelta(hours=9)-datetime.datetime.now()
+    print("Waiting time:", times_remain)
+    times_wait = times_remain/(api.rate_limit.remaining+1)
+    print("Time to wait for the next request:", times_wait.total_seconds())
+    print("Next Access time:",datetime.datetime.now()+times_wait)
+    time.sleep(times_wait.total_seconds())
+
+    if api.rate_limit.reset+datetime.timedelta(hours=9) > datetime.datetime.now():
         time.sleep((api.rate_limit.reset+datetime.timedelta(hours=9)-datetime.datetime.now()).seconds)
